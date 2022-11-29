56994b6ba0010836d3a03bc81be1fe55f28bc2f2
Pavan696 <96572303+Pavan696@users.noreply.github.com>
2022-11-29 14:05:45 +0530

    s=int(input()) l=list(map(int,input().split())) a,b=map(int,input().split()) k=[] for i in l:     if a<=i<=b:         k.append(i) if len(k)>0:     print(min(k)) else:     print(str(-1))

---
+++ minimum_between_A_and_B.py
@@ -0,0 +1,11 @@
+s=int(input())
+l=list(map(int,input().split()))
+a,b=map(int,input().split())
+k=[]
+for i in l:
+    if a<=i<=b:
+        k.append(i)
+if len(k)>0:
+    print(min(k))
+else:
+    print(str(-1))
