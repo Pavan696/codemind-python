d57de342ff120c7241fd1d3152a7801689c38e6b
Pavan696 <96572303+Pavan696@users.noreply.github.com>
2022-11-30 14:11:49 +0530

    import math def is_prime(a):     if a>1:         for i in range(2,int(math.sqrt(a)+1)):             if a%i==0:                 return False                 break     if a==1:         return False     return True
    a=int(input())
    b=int(input())
    c=0
    for i in range(a,b+1):
        if is_prime(i)==True:
            c+=1
    print(c)

---
+++ Primes_in_range.py
@@ -0,0 +1,18 @@
+import math
+def is_prime(a):
+    if a>1:
+        for i in range(2,int(math.sqrt(a)+1)):
+            if a%i==0:
+                return False
+                break
+    if a==1:
+        return False
+    return True
+    
+a=int(input())
+b=int(input())
+c=0
+for i in range(a,b+1):
+    if is_prime(i)==True:
+        c+=1
+print(c)
