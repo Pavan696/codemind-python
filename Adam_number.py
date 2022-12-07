b294b414c5cd0bd677a897162191bcff2135427d
Pavan696 <96572303+Pavan696@users.noreply.github.com>
2022-12-07 14:49:28 +0530

    def rev(x):     c=0     while x!=0:         r=x%10         c=c*10+r         x//=10     return c a=int(input()) c=rev(a) if a**2 == rev(c**2):     print(True) else:     print(False)

--- Adam_number.py
+++ Adam_number.py
@@ -1,14 +1,13 @@
-def rev(a):
-    s=0
-    while a!=0:
-        r=a%10
-        s=s*10+r
-        a//=10
-    return s
-
+def rev(x):
+    c=0
+    while x!=0:
+        r=x%10
+        c=c*10+r
+        x//=10
+    return c
 a=int(input())
 c=rev(a)
-if a**2==rev(c**2):
+if a**2 == rev(c**2):
     print(True)
 else:
     print(False)
