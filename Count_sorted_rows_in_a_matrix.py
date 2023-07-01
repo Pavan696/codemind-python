1136e7255a25b171722af57be79c3bb56fee941e
Pavan696 <96572303+Pavan696@users.noreply.github.com>
2023-07-01 10:50:32 +0530

    a,b=map(int,input().split()) c=0 for i in range(a):     l=list(map(int,input().split()))     if sorted(l)==l or sorted(l,reverse=True)==l:         c+=1 print(c)

--- Count_sorted_rows_in_a_matrix.py
+++ Count_sorted_rows_in_a_matrix.py
@@ -1,11 +1,8 @@
-def row_sor(l):
-    m=sorted(l)
-    n=sorted(l,reverse=True)
-    return l==m or l==n
+
 a,b=map(int,input().split())
-s=0
+c=0
 for i in range(a):
-    x=list(map(int,input().split()))
-    if row_sor(x):
-        s+=1
-print(s)
+    l=list(map(int,input().split()))
+    if sorted(l)==l or sorted(l,reverse=True)==l:
+        c+=1
+print(c)
