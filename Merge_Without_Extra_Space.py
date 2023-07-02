0a11ca8dc459cda7c916183ed41a9f365bef378a
Pavan696 <96572303+Pavan696@users.noreply.github.com>
2023-07-02 08:26:23 +0530

    x=int(input()) for i in range(x):     n,m=map(int,input().split())     a=list(map(int,input().split()))     b=list(map(int,input().split()))     l=[]     for k in range(n):         b.append(a[k])     print(*sorted(b))

--- Merge_Without_Extra_Space.py
+++ Merge_Without_Extra_Space.py
@@ -1,11 +1,9 @@
-a=int(input())
-for i in range(a):
-    b,c=map(int,input().split())
-    b1=list(map(int,input().split()))
-    c1=list(map(int,input().split()))
-    d=[]
-    for i in range(b):
-        d.append(b1[i])
-    for i in range(c):
-        d.append(c1[i])
-    print(*sorted(d))
+x=int(input())
+for i in range(x):
+    n,m=map(int,input().split())
+    a=list(map(int,input().split()))
+    b=list(map(int,input().split()))
+    l=[]
+    for k in range(n):
+        b.append(a[k])
+    print(*sorted(b))
