02e69ae1a5ffa6c4c6dfcdeb94a4713743853f46
Pavan696 <96572303+Pavan696@users.noreply.github.com>
2023-07-03 09:54:09 +0530

    def is_per(i):     j=int(i**0.5)     if j*j == i:         return True     else:         return False a=int(input()) l=list(map(int,input().split())) b=0 for i in l:     if is_per(i):         b+=i print(b)

--- Program_to_find_the_sum_of_perfect_square_elements_in_an_array.py
+++ Program_to_find_the_sum_of_perfect_square_elements_in_an_array.py
@@ -1,9 +1,15 @@
-n=int(input())
-a=list(map(int,input().split()))
-s=0
-for i in a:
-    b=i**0.5
-    c=int(b)
-    if c==b:
-        s+=i
-print(s)
+def is_per(i):
+    j=int(i**0.5)
+    if j*j == i:
+        return True
+    else:
+        return False
+a=int(input())
+l=list(map(int,input().split()))
+b=0
+for i in l:
+    if is_per(i):
+        b+=i
+print(b)
+
+    
